9edaa1d595f3ac4e1043d0503a90a3d1c7ba322b
klozz <klozz707@gmail.com>
2017-10-13 20:00:43 -0400

    RainbowUnicorn: add Rainbow Settings
    Change-Id: I87f2aefafa6d8f1d9c472823414eb436931883a2
    Signed-off-by: klozz <klozz707@gmail.com>
    Signed-off-by: klozz <shaclone707@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -167,5 +167,6 @@ public class Settings extends SettingsActivity {
     public static class SystemDashboardActivity extends SettingsActivity {}
     public static class SupportDashboardActivity extends SettingsActivity {}
     public static class SMQQtiFeedbackActivity extends SettingsActivity { /* empty */ }
+    public static class RainbowUnicornActivity extends SettingsActivity {}
 
 }

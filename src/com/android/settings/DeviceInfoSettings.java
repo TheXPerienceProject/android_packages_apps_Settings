59296b86829ab82abac5ed3809c26c117e8f066b
klozz <klozz707@gmail.com>
2017-10-23 06:16:47 -0400

    Settings: Add XPerience version to "About phone"
    Change-Id: Ib91d5d288fe55b1d18027dcd7b087afb69ac483b
    Signed-off-by: klozz <klozz707@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -39,6 +39,7 @@ import com.android.settings.deviceinfo.SELinuxStatusPreferenceController;
 import com.android.settings.deviceinfo.SafetyInfoPreferenceController;
 import com.android.settings.deviceinfo.SecurityPatchPreferenceController;
 import com.android.settings.deviceinfo.XPerienceUpdaterPreferenceController;
+import com.android.settings.deviceinfo.XPerienceVersionPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -129,6 +130,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         controllers.add(new FirmwareVersionPreferenceController(context, lifecycle));
         controllers.add(new RegulatoryInfoPreferenceController(context));
         controllers.add(new DeviceModelPreferenceController(context, fragment));
+		controllers.add(new XPerienceVersionPreferenceController(context));
         controllers.add(new SecurityPatchPreferenceController(context));
         controllers.add(new FccEquipmentIdPreferenceController(context));
         controllers.add(new SELinuxStatusPreferenceController(context));

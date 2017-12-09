f976bc831a89d5585a47a32807b0a7e7ec986587
Chris <chris@cypheros.co>
2017-12-09 17:34:28 -0500

    Settings: Switch battery light to a PreferenceController
    This keeps it in formation with the other preferences
    
    Change-Id: I858c308a6be1dfa38c7d4580f1f2008ae922c7e0

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -343,6 +343,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
                 context,
                 new AmbientDisplayConfiguration(context),
                 KEY_AMBIENT_DISPLAY));
+		controllers.add(new BatteryLightPreferenceController(context));
         return controllers;
     }
 

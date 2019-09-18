fd4d1721243b2681094e3ae55666cfdd1f3c6810
klozz <klozz@vivaldi.net>
2019-09-18 02:04:43 +0000

    Settings: Add XPerience entries into device info
    Change-Id: I9725cb21d53f7db074504b391daf6f51a015b662
    Signed-off-by: klozz <klozz@vivaldi.net>

---
+++ src/com/android/settings/deviceinfo/firmwareversion/SimpleBuildCodeNamePreferenceController.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.Build;
+import android.text.BidiFormatter;
+
+import com.android.settings.core.BasePreferenceController;
+
+public class SimpleBuildCodeNamePreferenceController extends BasePreferenceController {
+
+	private static final String KEY_XPERIENCE_CODENAME_PROP = "ro.xpe.codename";
+
+    public SimpleBuildCodeNamePreferenceController(Context context,
+            String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE_UNSEARCHABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return SystemProperties.get(KEY_XPERIENCE_CODENAME_PROP,
+                mContext.getString(R.string.unknown));
+    }
+}

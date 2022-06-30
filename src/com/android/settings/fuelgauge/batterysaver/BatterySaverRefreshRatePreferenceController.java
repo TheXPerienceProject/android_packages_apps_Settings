5b93366595524f726e4a5749c691d1e1ce568ccc
klozz <carlosj@klozz.dev>
2022-06-30 13:49:00 +0200

    fixes fixes fixes everywhere when the fking revert no revert the shit
    Signed-off-by: klozz <carlosj@klozz.dev>
    Change-Id: I135e50af971437b89562ba766fc97c1250aaeed5

--- src/com/android/settings/fuelgauge/batterysaver/BatterySaverRefreshRatePreferenceController.java
+++ src/com/android/settings/fuelgauge/batterysaver/BatterySaverRefreshRatePreferenceController.java
@@ -47,4 +47,7 @@ public class BatterySaverRefreshRatePreferenceController extends TogglePreferenc
         return mContext.getResources().getBoolean(R.bool.config_show_refresh_rate_controls)
                         ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
+
+    @Override
+    public abstract int getSliceHighlightMenuRes();
 }

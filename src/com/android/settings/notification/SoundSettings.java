a1e3fd3b63c4f07a61bed44e1ddc19c9c805b503
klozz <carlosj@klozz.dev>
2022-10-03 23:51:37 +0200

    Revert "Settings: Add switch for linked ring and media notification volumes"
    This reverts commit 1f67e9708a7d397fb68963645ced2be57daa88ee.

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -269,8 +269,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new EmergencyTonePreferenceController(context, fragment, lifecycle);
         final VibrateIconPreferenceController vibrateIconPreferenceController =
                 new VibrateIconPreferenceController(context, fragment, lifecycle);
-        final LinkedVolumesPreferenceController linkedVolumesPreferenceController =
-                new LinkedVolumesPreferenceController(context, fragment, lifecycle);
 
         controllers.add(dialPadTonePreferenceController);
         controllers.add(screenLockSoundPreferenceController);
@@ -281,7 +279,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         controllers.add(dockAudioMediaPreferenceController);
         controllers.add(bootSoundPreferenceController);
         controllers.add(emergencyTonePreferenceController);
-        controllers.add(linkedVolumesPreferenceController);
         controllers.add(new PreferenceCategoryController(context,
                 "other_sounds_and_vibrations_category").setChildren(
                 Arrays.asList(dialPadTonePreferenceController,
@@ -292,8 +289,7 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                         vibrateIconPreferenceController,
                         dockAudioMediaPreferenceController,
                         bootSoundPreferenceController,
-                        emergencyTonePreferenceController,
-                        linkedVolumesPreferenceController)));
+                        emergencyTonePreferenceController)));
 
         return controllers;
     }

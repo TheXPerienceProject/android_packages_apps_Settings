26dde2374f8b679993a24edece52fb88979505f9
klozz <carlosj@klozz.dev>
2023-04-20 23:44:40 -0600

    Revert "Revert "Revert "Settings: Add switch for linked ring and media notification volumes"""
    This reverts commit dc1410fd482ef28b5458fbb66e388be20a8bc66d.
    
    Change-Id: I7ab0ecafcb091fdcbc7c2832f585aa904736d097
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -272,8 +272,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                 new EmergencyTonePreferenceController(context, fragment, lifecycle);
         final VibrateIconPreferenceController vibrateIconPreferenceController =
                 new VibrateIconPreferenceController(context, fragment, lifecycle);
-        final LinkedVolumesPreferenceController linkedVolumesPreferenceController =
-                new LinkedVolumesPreferenceController(context, fragment, lifecycle);
 
         controllers.add(dialPadTonePreferenceController);
         controllers.add(callConnectedTonePreferenceController);
@@ -285,7 +283,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         controllers.add(dockAudioMediaPreferenceController);
         controllers.add(bootSoundPreferenceController);
         controllers.add(emergencyTonePreferenceController);
-        controllers.add(linkedVolumesPreferenceController);
         controllers.add(new PreferenceCategoryController(context,
                 "other_sounds_and_vibrations_category").setChildren(
                 Arrays.asList(dialPadTonePreferenceController,
@@ -297,8 +294,7 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
                         vibrateIconPreferenceController,
                         dockAudioMediaPreferenceController,
                         bootSoundPreferenceController,
-                        emergencyTonePreferenceController,
-                        linkedVolumesPreferenceController)));
+                        emergencyTonePreferenceController)));
 
         return controllers;
     }

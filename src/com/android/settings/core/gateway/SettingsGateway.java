b086f4208f2168f6841bbd847ae14909967d1505
cjh1249131356 <cjh1249131356@gmail.com>
2024-01-06 22:36:19 +0800

    Settings: Implement RefreshRateManager [2/2]
    Includes:
    - Per-app refresh rate config
    - Extreme refresh rate (Force all apps to run in maximum refresh rate)
    - Temp refresh rate allowed (Used in MEMC)
    - Refresh rate QS tile
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -23,6 +23,7 @@ import com.android.settings.MainClear;
 import com.android.settings.MainClearConfirm;
 import com.android.settings.ResetNetwork;
 import com.android.settings.Settings;
+import com.android.settings.Settings.ScreenRefreshRateActivity;
 import com.android.settings.TestingSettings;
 import com.android.settings.TrustedCredentialsSettings;
 import com.android.settings.accessibility.AccessibilityDetailsSettingsFragment;
@@ -218,6 +219,7 @@ import com.android.settings.wifi.details.WifiNetworkDetailsFragment;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
+import mx.xperience.settings.display.refreshrate.ScreenRefreshRateFragment;
 
 import mx.xperience.unicorn.Rainbow;
 
@@ -429,6 +431,7 @@ public class SettingsGateway {
             ToggleScreenMagnificationPreferenceFragment.class.getName(),
             FlashNotificationsPreferenceFragment.class.getName(),
             Rainbow.class.getName(),
+            ScreenRefreshRateFragment.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -478,5 +481,6 @@ public class SettingsGateway {
             Settings.RainbowActivity.class.getName(),
             Settings.FirmwareVersionActivity.class.getName(),
             Settings.WallpaperSettingsActivity.class.getName(),
+            Settings.ScreenRefreshRateActivity.class.getName(),
     };
 }

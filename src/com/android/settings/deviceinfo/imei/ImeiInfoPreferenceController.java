0dc7e91ee461970edecb32a392ce3b55efe49627
althafvly <althafvly@gmail.com>
2024-06-27 21:19:52 +0530

    Revert "[Sim UI enhancement] remove the "Tap to show info""
    This reverts commit c6eba747db60b044999e29bbf96643eb41799c3c.
    
    Change-Id: Ib8403ca842a4655d1526b27f1965feeee336c687
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
+++ src/com/android/settings/deviceinfo/imei/ImeiInfoPreferenceController.java
@@ -42,6 +42,7 @@ import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
+import com.android.settings.deviceinfo.PhoneNumberSummaryPreference;
 import com.android.settings.deviceinfo.simstatus.SlotSimStatus;
 import com.android.settings.network.SubscriptionUtil;
 import com.android.settings.network.telephony.MobileNetworkUtils;
@@ -163,6 +164,11 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
         updatePreference(preference, keyToSlotIndex(preference.getKey()));
     }
 
+    @Override
+    public CharSequence getSummary() {
+        return mContext.getString(R.string.device_info_protected_single_press);
+    }
+
     private CharSequence getSummary(int simSlot) {
         final int phoneType = getPhoneType(simSlot);
         return phoneType == PHONE_TYPE_CDMA ? mTelephonyManager.getMeid(simSlot)
@@ -220,7 +226,7 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
             return;
         }
         preference.setTitle(getTitle(simSlot));
-        preference.setSummary(getSummary(simSlot));
+        preference.setSummary(getSummary());
     }
 
     private String getImei(int slot) {
@@ -315,7 +321,7 @@ public class ImeiInfoPreferenceController extends BasePreferenceController {
 
     @VisibleForTesting
     Preference createNewPreference(Context context) {
-        return new Preference(context);
+        return new PhoneNumberSummaryPreference(context);
     }
 
     private int makeRadioVersion(int major, int minor) {

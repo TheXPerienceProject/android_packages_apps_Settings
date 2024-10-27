d7603d243cae252468fb9a3af70d9c0a966e15d7
klozz <carlosj@klozz.dev>
2024-10-27 17:42:40 -0600

    Reapply "add RainbowUnicorn settings"
    This reverts commit f98228f99b71641444f5fd41e8b01879d589339b.
    
    Change-Id: Ib77a707555bbbde3628c1976227fa5ffdc99b2a6
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -470,6 +470,7 @@ public class Settings extends SettingsActivity {
     public static class StorageDashboardActivity extends SettingsActivity {}
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
+    public static class RainbowActivity extends SettingsActivity {}
 
     /**
      * Activity for MediaControlsSettings
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -205,6 +205,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import mx.xperience.unicorn.Rainbow;
+
 public class SettingsGateway {
 
     /**
@@ -399,6 +401,7 @@ public class SettingsGateway {
             CellularSecuritySettingsFragment.class.getName(),
             AccessibilityHearingAidsFragment.class.getName(),
             HearingDevicePairingFragment.class.getName(),
+            Rainbow.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -445,5 +448,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.RainbowActivity.class.getName(),
     };
 }

c52e12a40107cd92dc7fa298292ab7a935b82f61
klozz <carlosj@klozz.dev>
2024-10-27 17:42:40 -0600

    Reapply "add RainbowUnicorn settings"
    This reverts commit f98228f99b71641444f5fd41e8b01879d589339b.
    
    Change-Id: Ib77a707555bbbde3628c1976227fa5ffdc99b2a6
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -217,6 +217,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import mx.xperience.unicorn.Rainbow;
+
 public class SettingsGateway {
 
     /**
@@ -422,6 +424,7 @@ public class SettingsGateway {
             ContentProtectionPreferenceFragment.class.getName(),
             ToggleScreenMagnificationPreferenceFragment.class.getName(),
             FlashNotificationsPreferenceFragment.class.getName(),
+            Rainbow.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -468,5 +471,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.RainbowActivity.class.getName(),
     };
 }

5abb9b45157f977ee03c2925689f2cb897f6c704
COSMIC <rayavanindra@gmail.com>
2024-12-13 15:30:54 +0530

    fixup! Settings: Introduce App Lock
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/com/android/settings/security/applock/AppLockPackageConfigFragment.kt
+++ src/com/android/settings/security/applock/AppLockPackageConfigFragment.kt
@@ -71,7 +71,7 @@ class AppLockPackageConfigFragment : DashboardFragment() {
         AppLockHideAppPC(context, packageInfo.packageName, lifecycleScope)
     )
 
-    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.VOLTAGE
+    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.RAINBOW_UNICORN
 
     override protected fun getPreferenceScreenResId() = R.xml.app_lock_package_config_settings
 
--- src/com/android/settings/security/applock/AppLockSettingsFragment.kt
+++ src/com/android/settings/security/applock/AppLockSettingsFragment.kt
@@ -40,7 +40,7 @@ class AppLockSettingsFragment : DashboardFragment(),
 
     override protected fun getPreferenceScreenResId() = R.xml.app_lock_settings
 
-    override fun getMetricsCategory() = MetricsProto.MetricsEvent.VOLTAGE
+    override fun getMetricsCategory() = MetricsProto.MetricsEvent.RAINBOW_UNICORN
 
     override protected fun getLogTag() = TAG
 

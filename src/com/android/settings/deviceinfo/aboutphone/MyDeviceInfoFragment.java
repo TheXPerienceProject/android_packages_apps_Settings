aa0340d121308f29658348ab46f58e76a865db96
klozz <carlosj@klozz.dev>
2025-07-05 03:16:30 -0600

    MyDeviceInfoFragment: addres NPE on PhoneNumberPreferenceController
    Change-Id: I40519b22ef2b05666bc6c48471aa526872fcdbee
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -94,9 +94,18 @@ public class MyDeviceInfoFragment extends DashboardFragment
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        use(DeviceNamePreferenceController.class).setHost(this /* parent */);
+        // Others 
+        use(DeviceNamePreferenceController.class).setHost(this);
         mBuildNumberPreferenceController = use(BuildNumberPreferenceController.class);
         mBuildNumberPreferenceController.setHost(this /* parent */);
+        // PhoneNumber: asign a variable value first
+        PhoneNumberPreferenceController phoneCtrl =
+                use(PhoneNumberPreferenceController.class);
+        if (phoneCtrl != null) {
+            phoneCtrl.init(getSettingsLifecycle());
+        } else {
+            Log.e(LOG_TAG, "PhoneNumberPreferenceController not disponible");
+        }
     }
 
     @Override

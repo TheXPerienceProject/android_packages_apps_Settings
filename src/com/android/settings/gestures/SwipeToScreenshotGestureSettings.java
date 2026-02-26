635c3a60f2841f0e935acc2ffeaf293c3d4ef829
klozz <carlosj@klozz.dev>
2026-02-25 19:47:05 -0600

    fix metrics
    Change-Id: I8618e137cf4f13a1757375a91cc94f6bb5e50feb
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/com/android/settings/gestures/SwipeToScreenshotGestureSettings.java
+++ src/com/android/settings/gestures/SwipeToScreenshotGestureSettings.java
@@ -44,7 +44,7 @@ public class SwipeToScreenshotGestureSettings extends DashboardFragment {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.VOLTAGE;
+        return MetricsEvent.RAINBOW_UNICORN;
     }
 
     @Override

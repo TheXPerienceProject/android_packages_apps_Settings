a495ae418c29a4c7520bc195957362afa908a36d
klozz <klozz@vivaldi.net>
2026-06-26 06:12:46 +0000

    DeviceStorageController: Fix storage size mismatch with advertised capacity
    formatBytes() was using binary divisors (1024) which caused storage
    to display as ~7.4 GB on an 8 GB device, mismatching the manufacturer
    advertised capacity.
    
    Switch to decimal divisors (1000) to match how manufacturers and
    Android Settings report storage size.
    
    Signed-off-by: klozz <klozz@vivaldi.net>

--- src/com/android/settings/deviceinfo/xperience/DeviceStorageController.java
+++ src/com/android/settings/deviceinfo/xperience/DeviceStorageController.java
@@ -112,10 +112,10 @@ public class DeviceStorageController extends BasePreferenceController {
     }
 
     private String formatBytes(long bytes) {
-        if (bytes < 1024) return bytes + " B";
-        int exp = (int) (Math.log(bytes) / Math.log(1024));
-        String pre = "KMGTPE".charAt(exp-1) + "B";
-        return String.format("%.1f %s", bytes / Math.pow(1024, exp), pre);
+        if (bytes < 1000) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(1000));
+        String pre = "KMGTPE".charAt(exp - 1) + "B";
+        return String.format("%.1f %s", bytes / Math.pow(1000, exp), pre);
     }
 
     private void openDeviceNameDialog() {

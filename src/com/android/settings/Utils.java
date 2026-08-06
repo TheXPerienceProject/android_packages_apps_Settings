acc25d514b237787846246ca8137e12005499641
klozz <carlosj@klozz.dev>
2026-08-06 06:39:50 -0600

    fix utils packagename
    Change-Id: I9b991dcf845c13092593d0f7901ce14035365b27
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1375,7 +1375,7 @@ public final class Utils extends com.android.settingslib.Utils {
     }
 
     public static String getAsiPackage(Context context) {
-        final boolean isGoogleAsiInstalled = com.android.internal.util.xperience.Utils.isPackageInstalled(context,
+        final boolean isGoogleAsiInstalled = com.android.internal.util.xperience.XperienceUtils.isPackageInstalled(context,
             context.getString(R.string.config_google_settingsintelligence_package_name));
         final String settingsIntelligencePkg = context.getString(isGoogleAsiInstalled
             ? R.string.config_google_settingsintelligence_package_name
